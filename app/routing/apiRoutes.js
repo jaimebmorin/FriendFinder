117a1909e5dc1b3c6652a240a57e306c599f6f71
Jaime <jaimebmorin@gmail.com>
2018-02-09 22:56:04 -0600

    coming along

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,3 +2,18 @@
 
 var friends = require("../data/friends");
 
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(tableData);
+    });    
+    app.post("/api/friends", function(req, res) {
+        friends.push(req.body);
+        res.json(true);
+    });
+    //for testing:
+    app.post("/api/clear", function() {
+        friends = [];
+        console.log(friends);
+      });
+}
+
