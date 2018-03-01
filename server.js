30dd3d4b80df098c1283539ac1d261822c71b6e9
Jaime <jaimebmorin@gmail.com>
2018-02-28 20:46:31 -0600

    working

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+app.listen(port, function() {
+    console.log("App listening on PORT: " + port);
   });
   
 
